446a97336ed0d47102b078d30152b312cdc29da8
Anjali Verma <anjaliverma728@gmail.com>
2019-10-26 15:09:15 +0530

    added stuff for complaint registration

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+document.getElementById('complaint-form').addEventListener("submit" , (e)=>{
+	console.log("Registered!");
+	var fname= document.getElementById("full_name");
+        var email = document.getElementById("email_address");
+        var uname = document.getElementById("user_name");
+        var pno= document.getElementById("phone_number");
+        var desc = document.getElementById("description");
+        var addno = document.getElementById("add_number");
+
+        if (fname.value==null || fname.value=="")
+        {
+            alert("Please Enter Your Full Name");
+            return false;
+        }
+        else if (email.value==null || email.value=="")
+        {
+            alert("Please Enter Your Email Address");
+            return false;
+        }
+        else if (uname.value==null || uname.value=="")
+        {
+            alert("Please Enter Your Username");
+            return false;
+        }
+        else if (pno.value==null || pno.value=="")
+        {
+            alert("Please Enter Your Present Address");
+            return false;
+        }
+        else if (desc.value==null || desc.value=="")
+        {
+            alert("Please Enter Your Description");
+            return false;
+        }
+
+        else if (addno.value==null || addno.value=="")
+        {
+            alert("Please Enter Your Adhar Number");
+            return false;
+        }
+        else {
+            true;
+        }
+}
+
+
 document.getElementById('login-form').addEventListener("submit" , (e)=>{
 			console.log("pressed!!");
       		var username = document.getElementById("uname");
@@ -21,6 +67,9 @@ document.getElementById('login-form').addEventListener("submit" , (e)=>{
       				})
       		}	
 });
+
+
+
 
 // function validate() {
 //       		var username = document.getElementById("uname");
